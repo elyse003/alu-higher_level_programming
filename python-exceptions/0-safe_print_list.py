619cbf3f676ac2fdfeed1370f90de21af44cb9e1
elyse003 <e.uyiringiy@alustudent.com>
2024-02-22 01:05:00 -0800

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end="")
-            num += 1
-        except IndexError:
-            break
-    print("")
-    Return
-
+    count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end='')
+            count += 1
+    except:
+        pass
+    print()
+    return count
